02ffa96ef14cb88cb6279fbe46ef22b2be1aa7f3
omkarpathak <opathak@uci.edu>
2024-02-10 14:53:50 -0800

    Bug fixes for a blank Github page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
-    <Router>
       <div>
         <Routes>
           <Route path="/" exact element= {<Home/>} />
           <Route path="*" element={<NotFound/>} />
         </Routes>
       </div>
-    </Router>
   );
 }
 
